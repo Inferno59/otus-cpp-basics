721ace29eaee367710763a6e70089869bd2078a4
i.zorich <i.zorich@reglab.ru>
2026-09-01 13:50:36 -0400

    С допами, прототип

--- lesson6_game/choose_number.cpp
+++ lesson6_game/choose_number.cpp
@@ -2,26 +2,28 @@
 #include <ios>
 #include "choose_number.hpp"
 
-ChooseNumber::ChooseNumber(std::string name) 
-  : user_name_(std::move(name))
+ChooseNumber::ChooseNumber() 
+  : user_name_("")
+  , max_value_(100)
   , target_value_(GetRandomValue())
-  , attempts_count_(0) {
-  std::cout << "user_name_: " << user_name_ << std::endl;
-}
+  , attempts_count_(0)
+  , scores_table_() {}
 
 int ChooseNumber::GetRandomValue() const {
   std::srand(std::time(nullptr)); // use current time as seed for random generator  
-  return std::rand() % 100;
+  return std::rand() % max_value_;
 }
 
 void ChooseNumber::Process() {
   CheckUserGuess();
-  ShowHighScoresTable();
+
+  if (!ShowHighScoresTable())
+    std::cerr << "Cannot add score of " << user_name_ << std::endl;
 }
 
 void ChooseNumber::CheckUserGuess() {
   int current_value = 0;
-  std::cout << "Enter your guess:" << std::endl;
+  std::cout << "Enter your guess (min: 0, max: "<< (max_value_ - 1) << "):" << std::endl;
 
   do {
     std::cin >> current_value;
@@ -41,49 +43,74 @@ void ChooseNumber::CheckUserGuess() {
   } while (true);
 }
 
-void ChooseNumber::ShowHighScoresTable() {
+bool ChooseNumber::ShowHighScoresTable() {
+  if (!AddToTable())
+    return false;
+
+  std::cout << "" << std::endl;
+  return ShowTable();
+}
+
+bool ChooseNumber::AddToTable() {
 	// Write new high score to the records table
-	{
-		// We should open the output file in the append mode - we don't want
-		// to erase previous results.
-		std::ofstream out_file{kHighScoresFilename.data(), std::ios_base::app};
-		if (!out_file.is_open()) {
-			std::cout << "Failed to open file for write: " << kHighScoresFilename << "!" << std::endl;
-			return;
-		}
-
-		// Append new results to the table:
-		out_file << user_name_ << ' ';
-		out_file << attempts_count_;
-		out_file << std::endl;
-	} // end of score here just to mark end of the logic block of code
-
-	// Read the high score file and print all results
-	{
-		std::ifstream in_file{kHighScoresFilename.data()};
-		if (!in_file.is_open()) {
-			std::cout << "Failed to open file for read: " << kHighScoresFilename << "!" << std::endl;
-			return;
-		}
-
-		std::cout << "High scores table:" << std::endl;
-
-		std::string username;
-		int high_score = 0;
-		while (true) {
-			// Read the username first
-			in_file >> username;
-			// Read the high score next
-			in_file >> high_score;
-			// Ignore the end of line symbol
-			in_file.ignore();
-
-			if (in_file.fail()) {
-				break;
-			}
-
-			// Print the information to the screen
-			std::cout << username << '\t' << high_score << std::endl;
-		}
+  // We should open the output file in the append mode - we don't want
+  // to erase previous results.
+  std::ofstream out_file{kHighScoresFilename.data(), std::ios_base::app};
+  if (!out_file.is_open()) {
+    std::cout << "Failed to open file for write: " << kHighScoresFilename << "!" << std::endl;
+    return false;
   }
+
+  // Append new results to the table:
+  out_file << user_name_ << ' ';
+  out_file << attempts_count_;
+  out_file << std::endl;
+
+  return true;
+}
+
+bool ChooseNumber::ShowTable() {
+	// Read the high score file and print all results
+  std::ifstream in_file{kHighScoresFilename.data()};
+  if (!in_file.is_open()) {
+    std::cout << "Failed to open file for read: " << kHighScoresFilename << "!" << std::endl;
+    return false;
+  }
+
+  std::cout << "High scores table:" << std::endl;
+
+  std::string username;
+  int high_score = 0;
+  while (true) {
+    // Read the username first
+    in_file >> username;
+    // Read the high score next
+    in_file >> high_score;
+    // Ignore the end of line symbol
+    in_file.ignore();
+
+    if (in_file.fail()) {
+      break;
+    }
+
+    auto it = scores_table_.find(username);
+    if (it == scores_table_.end())
+      scores_table_.emplace(username, high_score);
+    else if (it->second > high_score)
+      it->second = high_score;
+  }
+
+  for (auto& el : scores_table_)
+    std::cout << el.first << '\t' << el.second << std::endl;
+
+  return true;
+}
+
+void ChooseNumber::SetMaxValue(const int value) {
+  max_value_ = value;
+  target_value_ = GetRandomValue();
+}
+
+void ChooseNumber::SetUserName(std::string name) {
+  user_name_ = std::move(name);
 }
--- lesson6_game/choose_number.hpp
+++ lesson6_game/choose_number.hpp
@@ -4,22 +4,29 @@
 #include <iostream>
 #include <utility>
 #include <ctime>
+#include <map>
 
 class ChooseNumber final {
   public:
-    explicit ChooseNumber(std::string name);
+    explicit ChooseNumber();
 
     void Process();
-
+    void SetMaxValue(const int value);
+    void SetUserName(std::string name);
+    bool ShowTable();
+    
   private:
     static constexpr std::string_view kHighScoresFilename = "high_scores.txt";
     static constexpr int kMaxValue = 100;
 
     int GetRandomValue() const;
     void CheckUserGuess();
-    void ShowHighScoresTable();
+    bool ShowHighScoresTable();
+    bool AddToTable();
 
     std::string user_name_;
+    int max_value_;
     int target_value_;
     int attempts_count_;
+    std::map<std::string, int> scores_table_;
 };

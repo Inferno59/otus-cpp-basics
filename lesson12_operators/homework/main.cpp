da9b5df3e2e6e2c53b8d92be670db0b65747855d
i.zorich <i.zorich@reglab.ru>
2026-09-19 14:52:48 -0400

    Добавил реализацию вектора

--- lesson12_operators/homework/main.cpp
+++ lesson12_operators/homework/main.cpp
@@ -1,35 +1,26 @@
 #include <iostream>
 #include "vector/vector.hpp"
 
-class Test {
-public:
-  Test() {
-    std::cout << "Test ctor" << std::endl;
-  }
-
-  ~Test() {
-    std::cout << "Test dtor" << std::endl;
-  }  
-
-  friend std::ostream& operator<<(std::ostream& os, const Test& src) {
-    return os << "Test";
-  }
-  
-};
-
 int main() {
-  // my_containers::Vector<int> numbers({0, 1,2,3,4,5,6,7,8,9});
+  my_containers::Vector<int> numbers({0, 1,2,3,4,5,6,7,8,9});
 
-  // std::cout << numbers << std::endl;
-  // std::cout << numbers.size() << std::endl;
+  std::cout << numbers << std::endl;
+  std::cout << numbers.size() << std::endl;
 
-  Test test;
-  my_containers::Vector<Test> tests({test, test});
-  
-  // for(size_t i = 0; i < tests.size(); ++i) {
-  //   std::cout << tests[i] << std::endl;     
-  // }
-  //std::cout << tests << std::endl;
-  std::cout << tests.size() << std::endl;  
+  numbers.erase(3);
+  numbers.erase(4);
+  numbers.erase(5);
+
+  std::cout << numbers << std::endl;
+
+  numbers.insert(1, 10);
+  std::cout << numbers << std::endl;
+
+  numbers.insert(5, 20);
+  std::cout << numbers << std::endl;  
+
+  numbers.push_back(30);
+  std::cout << numbers << std::endl;
+
   return 0;
 }
--- lesson12_operators/homework/vector/vector.hpp
+++ lesson12_operators/homework/vector/vector.hpp
@@ -15,10 +15,7 @@ namespace  my_containers{
       explicit Vector(const T& value, size_t size);
       explicit Vector(std::initializer_list<T> values);
 
-      ~Vector() {
-        delete [] data;
-      }
-
+      ~Vector();
       Vector(const Vector& src);
       Vector(Vector&& src);
 
@@ -30,13 +27,17 @@ namespace  my_containers{
       };
 
       void push_back(const T& value);
+      void pop_back();
 
       Iterator insert(Iterator pos, const T& value);
+      void insert(size_t pos, const T& value);
       Iterator erase(Iterator pos);
       void erase(size_t pos);
 
       void reserved(size_t size);
       void resize(size_t size);
+      void clear();
+
       std::size_t max_size() const;
       std::size_t size() const;
 
@@ -48,15 +49,15 @@ namespace  my_containers{
       }
 
       const T& operator[](const T pos) const {
-          return data[pos];
-      }
-
+        return data[pos];
+      }      
     private:
       static constexpr uint16_t kDefaultSize = 10;
       static constexpr uint16_t kDefaultFactor = 2;
       size_t get_max_size(size_t size) {
         return size > kDefaultSize ? size * kDefaultFactor : kDefaultSize;
       }
+
       size_t size_;
       size_t max_size_;
       T* data;

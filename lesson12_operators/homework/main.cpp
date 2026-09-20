73014bc1513dcd0bfea1bc055bf01c527893982e
i.zorich <i.zorich@reglab.ru>
2026-09-20 12:10:03 -0400

    Сделал реализацию вектора

--- lesson12_operators/homework/main.cpp
+++ lesson12_operators/homework/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 #include "vector/vector.hpp"
 
+class Test {
+public:
+  Test() {
+    std::cout << "Test ctor" << std::endl;
+  }
+
+  ~Test() {
+    std::cout << "Test dtor" << std::endl;
+  }  
+
+  friend std::ostream& operator<<(std::ostream& os, const Test& src) {
+    return os << "Test";
+  }
+  
+};
+
 int main() {
   my_containers::Vector<int> numbers({0, 1,2,3,4,5,6,7,8,9});
 
@@ -22,5 +38,51 @@ int main() {
   numbers.push_back(30);
   std::cout << numbers << std::endl;
 
+  std::cout << "" << std::endl;
+  std::cout << "Show current vector by iterator loop" << std::endl;
+  for(auto it = numbers.begin(); it != numbers.end(); ++it)
+    std::cout << *it << " ";
+  std::cout << std::endl;
+
+  for(auto it = numbers.begin(); it != numbers.end();) {
+    it = numbers.erase(it);
+  }
+
+  std::cout << numbers.size() << std::endl;
+  for(auto it = numbers.begin(); it != numbers.end(); ++it)
+    std::cout << *it << " ";
+  std::cout << std::endl; 
+
+  auto it_begin = numbers.begin();
+  for(size_t i = 0; i < 60; ++i) {
+    it_begin = numbers.insert(it_begin, i);
+    ++it_begin;
+  }
+
+  std::cout << numbers.size() << std::endl;
+
+  for(auto it = numbers.begin(); it != numbers.end(); ++it)
+    std::cout << *it << " ";
+  std::cout << std::endl;  
+
+  std::cout << "" << std::endl;
+  std::cout << "Check ctor and operators" << std::endl;
+  my_containers::Vector<int> tmp{1,2,3,4,5};
+  
+  my_containers::Vector<int> copy_ctor_vector(tmp);
+  my_containers::Vector<int> move_ctor_vector_1(std::move(tmp));
+  my_containers::Vector<int> move_ctor_vector_2(std::move(my_containers::Vector<int>{9,8,7}));
+
+  std::cout << "copy_ctor_vector: " << copy_ctor_vector << std::endl;
+  std::cout << "move_ctor_vector_1: " << move_ctor_vector_1 << std::endl;
+
+  my_containers::Vector<int> copy_operator_vector;
+  copy_operator_vector = copy_ctor_vector;
+
+  my_containers::Vector<int> move_operator_vector;
+  move_operator_vector = std::move(copy_ctor_vector);
+
+  std::cout << "copy_operator_vector: " << copy_operator_vector << std::endl;
+  std::cout << "move_operator_vector: " << move_operator_vector << std::endl;  
   return 0;
 }
--- lesson12_operators/homework/vector/vector.hpp
+++ lesson12_operators/homework/vector/vector.hpp
@@ -8,30 +8,83 @@
 namespace  my_containers{
 
   template <typename T>
+  class vector_iterator {
+  public:
+      // === Обязательные typedef для совместимости с STL ===
+      using iterator_category = std::forward_iterator_tag; // Категория итератора
+      using value_type        = T;
+      using difference_type   = std::ptrdiff_t;
+      using pointer           = T*;
+      using reference         = T&;
+
+      // Конструктор
+      explicit vector_iterator(T* p) : ptr(p) {}
+
+      // Разыменование (доступ к значению)
+      reference operator*() const { return *ptr; }
+      
+      // Доступ к членам объекта (если T - это класс/структура)
+      pointer operator->() const { return ptr; }
+
+      // Префиксный инкремент (++it) - возвращает ссылку на себя
+      vector_iterator& operator++() {
+          ++ptr;
+          return *this;
+      }
+
+      T* get() { return ptr; }
+
+      // Постфиксный инкремент (it++) - возвращает копию старого состояния
+      vector_iterator operator++(int) {
+          vector_iterator tmp = *this;
+          ++(*this); // Вызываем префиксный инкремент
+          return tmp;
+      }
+
+      size_t operator-(const vector_iterator& b) const {
+        return ptr - b.ptr;
+      }
+
+      // Операторы сравнения
+      friend bool operator==(const vector_iterator& a, const vector_iterator& b) {
+          return a.ptr == b.ptr;
+      }
+      friend bool operator!=(const vector_iterator& a, const vector_iterator& b) {
+          return a.ptr != b.ptr;
+      }
+
+      bool operator>(const vector_iterator& b) {
+          return ptr > b.ptr;
+      }
+      bool operator<(const vector_iterator& b) {
+          return ptr < b.ptr;
+      }      
+    private:
+        T* ptr; // Внутренний указатель на текущий элемент
+  };
+
+  template <typename T>
   class Vector {
     public:
-      Vector();
+      explicit Vector();
       explicit Vector(const T& value);
       explicit Vector(const T& value, size_t size);
       explicit Vector(std::initializer_list<T> values);
 
       ~Vector();
-      Vector(const Vector& src);
-      Vector(Vector&& src);
+      Vector(const Vector<T>& src);
+      Vector(Vector<T>&& src);
 
-      Vector& operator=(const Vector& src);
-      Vector& operator=(Vector&& src);
-
-      class Iterator {
-
-      };
+      Vector<T>& operator=(const Vector<T>& src);
+      Vector<T>& operator=(Vector<T>&& src);
 
       void push_back(const T& value);
       void pop_back();
 
-      Iterator insert(Iterator pos, const T& value);
+      using iterator = vector_iterator<T>;
+      iterator insert(iterator& pos, const T& value);
       void insert(size_t pos, const T& value);
-      Iterator erase(Iterator pos);
+      iterator erase(iterator& pos);
       void erase(size_t pos);
 
       void reserved(size_t size);
@@ -41,26 +94,32 @@ namespace  my_containers{
       std::size_t max_size() const;
       std::size_t size() const;
 
-      Iterator begin();
-      Iterator end();
+      iterator begin() { return iterator{data_}; }
+      iterator end() {return iterator{data_ + size_}; };
 
       T& operator[](const T pos) {
-        return data[pos];
+        return data_[pos];
       }
 
       const T& operator[](const T pos) const {
-        return data[pos];
+        return data_[pos];
       }      
     private:
       static constexpr uint16_t kDefaultSize = 10;
       static constexpr uint16_t kDefaultFactor = 2;
+
+      void shift_to_right_from_idx(int idx);
+      void shift_to_left_from_idx(int idx);
+
+      void clear_internal_memory();
+      T* allocate(size_t size);
       size_t get_max_size(size_t size) {
         return size > kDefaultSize ? size * kDefaultFactor : kDefaultSize;
       }
 
       size_t size_;
       size_t max_size_;
-      T* data;
+      T* data_;
   };
 
   template <typename T>
